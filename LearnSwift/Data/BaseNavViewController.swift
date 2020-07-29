7f71631d84295ed3c659d7f7444f815434c82882
庞宏侠 <1271318591@qq.com>
2020-07-29 17:08:15 +0800

    first commit

---
+++ LearnSwift/Data/BaseNavViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseNavViewController.swift
+//  LearnSwift
+//
+//  Created by 庞宏侠 on 2020/7/28.
+//  Copyright © 2020 jiuhuikeji. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
